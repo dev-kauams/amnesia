99c824b20e6fc8359b7a2ee95e311f9e615728d4
dev-kauams <kauams.profissional@gmail.com>
2026-09-18 12:11:04 -0300

    Refatora a rota PUT para atualização de tópicos, melhorando a validação de dados e a lógica de resposta

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,18 +3,13 @@ import express from 'express'
 const routes = express.Router()
 
 
-const topicos = [
-    { id: 1, titulo: "Aprender React", },
-    { id: 2, titulo: "Aprender Node.js", },
-    { id: 3, titulo: "Aprender TypeScript", }    
-                ]
+const topicos = 
+    [
+        { id: 1, titulo: "Aprender React", },
+        { id: 2, titulo: "Aprender Node.js", },
+        { id: 3, titulo: "Aprender TypeScript", }    
+    ]
 
-// routes.get("/topicos", (req, res) => {
-//     return res.json([{
-//         id: 1,
-//         titulo: "Aprender React",
-//     }])
-// })
 
 routes.get("/topicos", (req, res) => {
     return res.json(topicos)
@@ -31,15 +26,17 @@ routes.get("/topicos/:id", (req, res) => {
 routes.put("/topicos/:id", (req, res) => {
     const id = Number(req.params.id)
     const {titulo} = req.body
+    if(!titulo){
+        return res.status(400).json({message: "Os dados são obrigatórios"})
+    }
     const index = topicos.findIndex(item => item.id === id)
-    const status = index >= 0 ? 200 : 404
 
     if (index === -1) {
-        return res.status(status).json({message: "ID não encontrado."})
+        return res.status(404).json({message: "ID não encontrado."})
     }
 
     topicos[index] = {id: Number(id), titulo: titulo} 
-    return res.status(status).json(topicos[index])
+    return res.status(200).json(topicos[index])
 })
 
 export default routes
