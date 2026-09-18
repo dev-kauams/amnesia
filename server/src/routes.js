73541fddb4eeefda40e9f872e89f98a1a753b7ca
dev-kauams <kauams.profissional@gmail.com>
2026-09-18 13:31:06 -0300

    Implementa rota POST para criação de tópicos

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const routes = express.Router()
 
-
+// Array para teste
 const topicos = 
     [
         { id: 1, titulo: "Aprender React", },
@@ -10,24 +10,26 @@ const topicos =
         { id: 3, titulo: "Aprender TypeScript", }    
     ]
 
-
+// Rota GET Geral
 routes.get("/topicos", (req, res) => {
     return res.json(topicos)
 })
 
+// Rota GET Específica
 routes.get("/topicos/:id", (req, res) => {
-    const id = parseInt(req.params.id)
+    const id = Number(req.params.id)
     const topico = topicos.find(item => item.id === id)
     const status = topico ? 200 : 404
 
     return res.status(status).json(topico)
 })
 
+// Rota PUT
 routes.put("/topicos/:id", (req, res) => {
     const id = Number(req.params.id)
-    const {titulo} = req.body
+    const { titulo } = req.body
     if(!titulo){
-        return res.status(400).json({message: "Os dados são obrigatórios"})
+        return res.status(400).json({message: "Preencher os campos é obrigatório."})
     }
     const index = topicos.findIndex(item => item.id === id)
 
@@ -37,6 +39,24 @@ routes.put("/topicos/:id", (req, res) => {
 
     topicos[index] = {id: Number(id), titulo: titulo} 
     return res.status(200).json(topicos[index])
+})
+
+// Rota POST
+routes.post("/topicos", (req, res) => {
+    const { titulo } = req.body
+    if(!titulo){
+        return res.status(400).json({message: "Preencher os campos é obrigatório."})
+    }
+
+    const proximoId = topicos.length + 1
+    const novoTopico =
+        { 
+            id: proximoId,
+            titulo: titulo,
+        }
+
+    topicos.push(novoTopico)
+    return res.status(201).json(novoTopico)
 })
 
 export default routes
