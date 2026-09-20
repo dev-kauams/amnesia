f690cd5b12cac0eff2b867642394b5df797687a5
Kauã Mariano Santinho <kauams.profissional@gmail.com>
2026-09-19 22:44:05 -0300

    Implementa rota PATCH para atualização parcial  de tópicos Refatora rotas para tópicos, melhorando tratamento de erros Adicionado campo 'concluido' no objeto Tópico

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,50 +5,92 @@ const routes = express.Router()
 // Array para teste
 const topicos = 
     [
-        { id: 1, titulo: "Aprender React", },
-        { id: 2, titulo: "Aprender Node.js", },
-        { id: 3, titulo: "Aprender TypeScript", }    
+        { id: 1, titulo: "Aprender React", concluido: false },
+        { id: 2, titulo: "Aprender Node.js", concluido: false },
+        { id: 3, titulo: "Aprender TypeScript", concluido: true }    
     ]
 
 // Rota GET Geral
 routes.get("/topicos", (req, res) => {
-    return res.json(topicos)
+    return res.status(200).json(topicos)
 })
 
 // Rota GET Específica
 routes.get("/topicos/:id", (req, res) => {
     const id = Number(req.params.id)
     const topico = topicos.find(item => item.id === id)
-    const status = topico ? 200 : 404
 
-    return res.status(status).json(topico)
+    // Tratamento de erro
+    if(!topico){
+        return res.status(404).json({ message: "[ERRO]: Objeto não encontrado." })
+    }
+
+    return res.status(200).json(topico)
 })
 
 // Rota PUT
 routes.put("/topicos/:id", (req, res) => {
     const id = Number(req.params.id)
-    const { titulo } = req.body
-    if(!titulo){
+    const { titulo, concluido } = req.body
+    const index = topicos.findIndex(item => item.id === id)
+
+    // Tratamento de erro
+    if(!titulo || concluido === undefined){
         return res.status(400).json({ message: "[ATENÇÃO]: É obrigatório preencher todos os campos." })
     }
-    const index = topicos.findIndex(item => item.id === id)
 
     if (index === -1) {
         return res.status(404).json({ message: "[ERRO]: Objeto não encontrado." })
     }
 
-    topicos[index] = {id: Number(id), titulo: titulo} 
+    topicos[index] = 
+    { 
+        id: Number(id), 
+        titulo: titulo, 
+        concluido: concluido
+    }
+
+    return res.status(200).json(topicos[index])
+})
+
+
+// Rota PATCH
+routes.patch("/topicos/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const { titulo, concluido } = req.body
+    const index = topicos.findIndex(item => item.id === id)
+
+    // Tratamento de erro
+    if(titulo !== undefined && (typeof titulo !== "string" || titulo.trim() === "")){
+        return res.status(400).json({ message: "[ATENÇÃO]: O título deve ser um texto válido." })
+    }
+    if(!titulo && concluido === undefined){
+        return res.status(400).json({ message: "[ATENÇÃO]: É obrigatório preencher ao menos 1(um) dos campos devem ser preenchidos." })
+    }
+    if(index === -1){
+        return res.status(404).json({ message: "[ERRO]: Objeto não encontrado."})
+    }
+
+    topicos[index] = 
+    {
+        ...topicos[index],
+        ...req.body,
+        id: id
+    }
+
     return res.status(200).json(topicos[index])
 })
 
 // Rota POST
 routes.post("/topicos", (req, res) => {
     const { titulo } = req.body
+    const proximoId = topicos.length + 1
+
+    // Tratamento de erro
     if(!titulo){
         return res.status(400).json({ message: "[ATENÇÃO]: É obrigatório preencher todos os campos." })
     }
 
-    const proximoId = topicos.length + 1
     const novoTopico =
         { 
             id: proximoId,
@@ -59,10 +101,13 @@ routes.post("/topicos", (req, res) => {
     return res.status(201).json(novoTopico)
 })
 
+
 // Rota DELETE
 routes.delete("/topicos/:id", (req, res) => {
     const id = Number(req.params.id)
     const index = topicos.findIndex(item => item.id === id)
+
+    // Tratamento de erro
     if(index === -1){
         return res.status(404).json({ message: "[ERRO]: Objeto não encontrado." })
     }    
